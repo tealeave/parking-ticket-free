2ff84dc9544a05bc49ee4e2fe261924b231bb319
tealeave <tealeave@gmail.com>
2023-08-10 09:33:58 -0700

    evaluation update

--- la_parking_torch.py
+++ la_parking_torch.py
@@ -25,12 +25,11 @@ BATCH_SIZE = 128
 EPOCHS = 10
 LEARNING_RATE = 0.001
 DATA_PATH = "/dfs6/pub/ddlin/projects/parking_citation/top10_violations_2020_2022.csv"
-# Set memory management parameters for PyTorch CUDA
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb=256,min_split_size_mb=4,not_reuse_first=1'
 
 
-
 class SimpleNN(nn.Module):
+    """A simple feedforward neural network."""
     def __init__(self, input_dim, output_dim):
         super(SimpleNN, self).__init__()
         self.layer1 = nn.Linear(input_dim, 128)
@@ -43,15 +42,8 @@ class SimpleNN(nn.Module):
         x = self.layer3(x)
         return x
 
-def run():
-    start_time = time.time()
-
-    # Check CUDA availability
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"CUDA available: {torch.cuda.is_available()}")
-    print(f"Using device: {device}")
-
-    # Data Preparation
+def load_data():
+    """Loads and preprocesses the dataset."""
     data = pd.read_csv(DATA_PATH)
     features = ['date of week', 'Issue Hour', 'Cluster']
     X = data[features]
@@ -60,17 +52,31 @@ def run():
     encoded_columns = ohe.get_feature_names_out(features)
     X = pd.DataFrame(X_encoded, columns=encoded_columns)
     y = LabelEncoder().fit_transform(data['Violation Description'])
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def run():
+    start_time = time.time()
+
+    # Check CUDA availability
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"CUDA available: {torch.cuda.is_available()}")
+    print(f"Using device: {device}")
+
+    X_train, X_test, y_train, y_test = load_data()
     X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32).to(device)
     y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
     train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
+    # Convert test data to tensors
+    X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32).to(device)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.long).to(device)
+
     # Model Definition & Training
     model = SimpleNN(X_train.shape[1], len(set(y_train))).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    epoch_precisions, epoch_recalls = [], []
+
     for epoch in range(EPOCHS):
         for data, target in train_loader:
             optimizer.zero_grad()
@@ -80,24 +86,33 @@ def run():
             optimizer.step()
         print(f"Epoch {epoch+1}/{EPOCHS}, Loss: {loss.item()}")
 
-        # Evaluation
+        # Training Evaluation
         with torch.no_grad():
-            y_pred = model(X_train_tensor)
-            _, predicted = torch.max(y_pred, 1)
-            true_labels = y_train_tensor.cpu().numpy()
-            pred_labels = predicted.cpu().numpy()
-            precision = precision_score(true_labels, pred_labels, average='macro')
-            recall = recall_score(true_labels, pred_labels, average='macro')
-            epoch_precisions.append(precision)
-            epoch_recalls.append(recall)
-            accuracy = (predicted == y_train_tensor).sum().item() / len(y_train_tensor)
-            print(f"Training Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}")
+            y_pred_train = model(X_train_tensor)
+            _, predicted_train = torch.max(y_pred_train, 1)
+            true_labels_train = y_train_tensor.cpu().numpy()
+            pred_labels_train = predicted_train.cpu().numpy()
+            precision_train = precision_score(true_labels_train, pred_labels_train, average='macro')
+            recall_train = recall_score(true_labels_train, pred_labels_train, average='macro')
+            accuracy_train = (predicted_train == y_train_tensor).sum().item() / len(y_train_tensor)
+            print(f"Training Accuracy: {accuracy_train:.4f}, Precision: {precision_train:.4f}, Recall: {recall_train:.4f}")
+
+            # Test Evaluation
+            y_pred_test = model(X_test_tensor)
+            _, predicted_test = torch.max(y_pred_test, 1)
+            true_labels_test = y_test_tensor.cpu().numpy()
+            pred_labels_test = predicted_test.cpu().numpy()
+            precision_test = precision_score(true_labels_test, pred_labels_test, average='macro')
+            recall_test = recall_score(true_labels_test, pred_labels_test, average='macro')
+            accuracy_test = (predicted_test == y_test_tensor).sum().item() / len(y_test_tensor)
+            print(f"Test Accuracy: {accuracy_test:.4f}, Precision: {precision_test:.4f}, Recall: {recall_test:.4f}")
 
         torch.cuda.empty_cache()
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Total execution time: {elapsed_time:.2f} seconds")
+
 
 if __name__ == "__main__":
     run()
